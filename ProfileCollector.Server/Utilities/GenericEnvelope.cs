873fef2409bd9df639ab53637f40f9ab4dbe041f
Jeremiah Adeoye <jeremiah.adeoye@jaffatech.com>
2024-09-24 12:08:41 -0400

    seperated Envelope wrapper class to a new file

---
+++ ProfileCollector.Server/Utilities/GenericEnvelope.cs
@@ -0,0 +1,17 @@
+﻿namespace ProfileCollector.Server.Utilities
+{
+    public class Envelope<T>
+    {
+        public T Result { get; }
+        public string ErrorMessage { get; }
+        public DateTime TimeGenerated { get; }
+
+
+        protected internal Envelope(T result, string errorMessage)
+        {
+            Result = result;
+            ErrorMessage = errorMessage;
+            TimeGenerated = DateTime.UtcNow;
+        }
+    }
+}
