b19037aa81a031e240df01a96d83fe3313b9b7d5
Jeremiah Adeoye <jeremiah.adeoye@jaffatech.com>
2024-09-24 13:51:17 -0400

    added local and global exception handlers

---
+++ ProfileCollector.Domain/Entities/ExceptionLog.cs
@@ -0,0 +1,37 @@
+﻿using CSharpFunctionalExtensions;
+using ProfileCollector.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileCollector.Domain.Entities
+{
+    public class ExceptionLog : Entity<string>, IAggregateRoot
+    {
+        public string Message { get; private set; }
+        public string StackTrace { get; private set; }
+        public DateTime OccurredAt { get; private set; }
+        public string Path { get; private set; }
+        public string Method { get; private set; }
+
+        private ExceptionLog()
+        {
+        }
+
+        private ExceptionLog(string message, string stackTrace, DateTime occurredAt, string path, string method)
+        {
+            Message = message;
+            StackTrace = stackTrace;
+            OccurredAt = occurredAt;
+            Path = path;
+            Method = method;
+        }
+
+        public static ExceptionLog Create(string message, string stackTrace, DateTime occurredAt, string path, string method)
+        {
+            return new ExceptionLog(message, stackTrace, occurredAt, path, method);
+        } 
+    }
+}
---
+++ ProfileCollector.Server/Filters/ExceptionLoggerFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ProfileCollector.Domain.Entities;
+using ProfileCollector.Infrastructure.Interfaces;
+
+namespace ProfileCollector.Server.Filters
+{
+    public class ExceptionLoggerFilter : IAsyncExceptionFilter
+    {
+        private readonly IExceptionLogger _logger;
+        public ExceptionLoggerFilter(IExceptionLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            var ex = context.Exception;
+
+            var log = ExceptionLog.Create(ex.Message, ex.StackTrace, DateTime.UtcNow, context.HttpContext.Request.Path, context.HttpContext.Request.Method);
+
+            await _logger.LogAsync(log);
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
