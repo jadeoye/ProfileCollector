332bc7a28561b178668901a473326a4b3c97d548
Jeremiah Adeoye <jeremiah.adeoye@jaffatech.com>
2024-09-24 19:40:31 -0400

    change for nullable stackTrace when handling exceptions

--- ProfileCollector.Domain/Entities/ExceptionLog.cs
+++ ProfileCollector.Domain/Entities/ExceptionLog.cs
@@ -11,7 +11,7 @@ namespace ProfileCollector.Domain.Entities
     public class ExceptionLog : Entity<string>, IAggregateRoot
     {
         public string Message { get; private set; }
-        public string StackTrace { get; private set; }
+        public string? StackTrace { get; private set; }
         public DateTime OccurredAt { get; private set; }
         public string Path { get; private set; }
         public string Method { get; private set; }
@@ -20,7 +20,7 @@ namespace ProfileCollector.Domain.Entities
         {
         }
 
-        private ExceptionLog(string message, string stackTrace, DateTime occurredAt, string path, string method)
+        private ExceptionLog(string message, string? stackTrace, DateTime occurredAt, string path, string method)
         {
             Message = message;
             StackTrace = stackTrace;
@@ -29,7 +29,7 @@ namespace ProfileCollector.Domain.Entities
             Method = method;
         }
 
-        public static ExceptionLog Create(string message, string stackTrace, DateTime occurredAt, string path, string method)
+        public static ExceptionLog Create(string message, string? stackTrace, DateTime occurredAt, string path, string method)
         {
             return new ExceptionLog(message, stackTrace, occurredAt, path, method);
         } 
